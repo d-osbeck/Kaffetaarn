70e7f784c4614104478d97b54ce76b7716eb0c98
ander <soer9428@edu.easj.dk>
2021-05-26 20:20:33 +0200

    Bacis setup af "Find vej.php" --> php template, php includes, links til css, metadata med SEO.

---
+++ Find vej.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+
+    <meta charset="utf-8">
+
+    <title>Find vej til Kaffetårn - café og spisested, Nykøbing F</title>
+    <meta name="description" content="Find vej til Kaffetårn - Hollands Gaard 30, 4800 Nykøbing Falster - Café og spisested.">
+
+    <script type="application/ld+json">
+        {
+            "@context": "https://schema.org",
+            "@type": "Restaurant",
+            "image": [
+                "https://example.com/photos/1x1/photo.jpg",
+                "https://example.com/photos/4x3/photo.jpg",
+                "https://example.com/photos/16x9/photo.jpg"
+            ],
+            "@id": "http://www.example.com/forside",
+            "name": "KAFFETÅR'N",
+            "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "Hollands Gaard 30",
+                "addressLocality": "Nykøbing Falster",
+                "postalCode": "4800"
+            },
+            "url": "http://www.example.com/findvej",
+            "telephone": "+4593946454",
+            "openingHoursSpecification": [
+                {
+                    "@type": "OpeningHoursSpecification",
+                    "dayOfWeek": [
+                        "Mandag",
+                        "Tirsdag",
+                        "Onsdag",
+                        "Torsdag"
+                    ],
+                    "opens": "11:00",
+                    "closes": "14:00"
+                },
+                {
+                    "@type": "OpeningHoursSpecification",
+                    "dayOfWeek": [
+                        "Fredag"
+                    ],
+                    "opens": "11:00",
+                    "closes": "15:00"
+                },
+                {
+                    "@type": "OpeningHoursSpecification",
+                    "dayOfWeek": [
+                        "Lørdag"
+                        ],
+                    "opens": "10:00",
+                    "closes": "15:00"
+                },
+                {
+                    "@type": "OpeningHoursSpecification",
+                    "dayOfWeek": [
+                        "Søndag"
+                        ],
+                    "opens": "16:00",
+                    "closes": "22:00"
+                }
+            ]
+        }
+    </script>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Camilla, Sandra, David & Søren.">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond&family=Oswald:wght@300;400;500&display=swap" rel="stylesheet">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+<?php include 'nav.php';?>
+
+
+
+<?php include 'footer.php';?>
+
+<script src="script.js"></script>
+
+</body>
+</html>

f2efd2653fe90f3b25dcf36e3e13b4d3323a4214
ander <soer9428@edu.easj.dk>
2021-05-27 14:07:39 +0200

    Opsætning af template for html og css til Om Kaffatår'n.

---
+++ omkaffetaarn.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+
+    <meta charset="utf-8">
+
+    <title>Dea og Kaffetårn - fra vandtårn til cafe</title>
+    <meta name="description" content="I midten af Nykøbing Falster, I bunden af det store gule vandtårn, ligger den lille hyggelige café - KAFFETÅR'N.">
+
+    <script type="application/ld+json">
+        {
+            "@context": "https://schema.org",
+            "@type": "Restaurant",
+            "image": [
+                "https://example.com/photos/1x1/photo.jpg",
+                "https://example.com/photos/4x3/photo.jpg",
+                "https://example.com/photos/16x9/photo.jpg"
+            ],
+            "@id": "http://www.example.com/forside",
+            "name": "KAFFETÅR'N",
+            "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "Hollands Gaard 30",
+                "addressLocality": "Nykøbing Falster",
+                "postalCode": "4800"
+            },
+            "url": "http://www.example.com/findvej",
+            "telephone": "+4593946454",
+            "openingHoursSpecification": [
+                {
+                    "@type": "OpeningHoursSpecification",
+                    "dayOfWeek": [
+                        "Mandag",
+                        "Tirsdag",
+                        "Onsdag",
+                        "Torsdag"
+                    ],
+                    "opens": "11:00",
+                    "closes": "14:00"
+                },
+                {
+                    "@type": "OpeningHoursSpecification",
+                    "dayOfWeek": [
+                        "Fredag"
+                    ],
+                    "opens": "11:00",
+                    "closes": "15:00"
+                },
+                {
+                    "@type": "OpeningHoursSpecification",
+                    "dayOfWeek": [
+                        "Lørdag"
+                    ],
+                    "opens": "10:00",
+                    "closes": "15:00"
+                },
+                {
+                    "@type": "OpeningHoursSpecification",
+                    "dayOfWeek": [
+                        "Søndag"
+                    ],
+                    "opens": "16:00",
+                    "closes": "22:00"
+                }
+            ]
+        }
+    </script>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Camilla, Sandra, David & Søren.">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/omkaffetaarn.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond&family=Oswald:wght@300;400;500&display=swap" rel="stylesheet">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+<?php include 'nav.php';?>
+
+
+
+<?php include 'footer.php';?>
+
+<script src="script.js"></script>
+<script src="https://kit.fontawesome.com/d7a71e7a7e.js" crossorigin="anonymous"></script>
+
+</body>
+</html>

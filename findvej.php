de9dab207ed4bcd4ccea6ebb61daa3a92bdd99d4
ander <soer9428@edu.easj.dk>
2021-05-31 08:52:37 +0200

    Ny font added. Rettelser til history section, på siden "omkaffetaarn". Padding, margin, position mm.

--- findvej.php
+++ findvej.php
@@ -74,7 +74,8 @@
     <link href="css/findvej.css" rel="stylesheet" type="text/css">
     <link href="css/styles.css" rel="stylesheet" type="text/css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond&family=Oswald:wght@300;400;500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Jost&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.typekit.net/alv5vit.css">
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
